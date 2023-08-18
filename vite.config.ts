af4eb37326e76a4600631ee67ff95b07da27dec3
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2023-08-18 13:09:21 +0100

    🚑

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   publicDir: process.env.NODE_ENV === 'production'
     ? '/portfolio/'
     : '/',
@@ -16,6 +15,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  
+  }
 })
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.export = {
     publicPath: process.env.NODE_ENV === 'production'
     ? '/portfolio/'
-    : '/'
+    : '/',
 }
