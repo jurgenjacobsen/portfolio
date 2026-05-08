dc77f7a1bb7aa0f9667172c93b4e07fc7c605181
Jürgen Jacobsen <jurgenjacobsen@outlook.com>
2026-05-08 19:52:58 +0100

    🚑 Remove typography plugin from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import typography from "@tailwindcss/typography";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(), typography],
+    plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
